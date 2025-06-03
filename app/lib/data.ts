66b50e32e92d3e739c44058fcbeca1077cab344a
Ttroublemaker <1614922765@qq.com>
2025-06-03 18:43:48 +0800

    测试

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,11 +18,11 @@ export async function fetchRevenue() {
 
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    let timeStart = performance.now();
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
-
+    data[0].revenue = performance.now() - timeStart;
     return data;
   } catch (error) {
     console.error('Database Error:', error);
