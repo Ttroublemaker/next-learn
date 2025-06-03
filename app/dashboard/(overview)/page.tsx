dfe3e6ec0a24451dfdae0b6989e0c297964588ec
Ttroublemaker <1614922765@qq.com>
2025-06-03 20:49:08 +0800

    dashboard force-dynamic

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -16,6 +16,8 @@ import {
 } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   // const revenue = await fetchRevenue();
   // const latestInvoices = await fetchLatestInvoices();
